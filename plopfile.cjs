f52e423b2c90512e5f44723ac3020d5fc84bb990
laoer536 <1547698569@qq.com>
2023-01-09 22:45:39 +0800

    feat: v2.1.3

--- plopfile.cjs
+++ plopfile.cjs
@@ -1,6 +1,6 @@
-module.exports= (plop)=> {
+module.exports = (plop) => {
     plop.setGenerator('creat-nuxt-template', {
-        description:'This is a quick template program to create nuxt template.',
+        description: 'This is a quick template program to create nuxt template.',
         prompts: [
             {
                 type: 'confirm',
@@ -11,18 +11,22 @@ module.exports= (plop)=> {
                 type: 'input',
                 name: 'fileName',
                 message: 'Set project name',
-                when:(res)=>res.nuxt3
+                when: (res) => res.nuxt3
             },
         ],
         actions: function (data) {
-            let actions = [];
+            let actions = ['Start to create nuxt3 template...'];
             if (data.nuxt3) {
                 actions.push({
                     type: 'addMany',
                     destination: data.fileName,
                     base: `templates/nuxt3`,
                     templateFiles: 'templates/nuxt3/**'
-                });
+                }, async function customAction(answers) {
+                    let successMsg = `The {{fileName}} project is created, now you can see it at ${process.cwd()}/{{fileName}}.`;
+                    // you can use plop.renderString to render templates
+                    return plop.renderString(successMsg, answers);
+                }, 'You need to open this project according to the above path and execute "yarn"','Then execute "yarn dev" to run the project');
             }
             return actions;
         }
